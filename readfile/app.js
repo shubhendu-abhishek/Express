62c3c9ba88c6e130f3b17ad106943998264518f1
Shubhendu Abhishek <shubhendu.abhishek@above-inc.com>
2017-10-06 19:33:37 +0530

    csv file

--- readfile/app.js
+++ readfile/app.js
@@ -2,28 +2,37 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var json2csv = require('json2csv');
 var app = express();
 const https = require('https');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-var htmlparser = require("htmlparser2");
-var getElement = require('get-element');
 var urls = [];
-var document = require('html-element').document;
+
+var fields = ['S.No', 'href'];
 app.get('/scrape', function (req, res) {
     request.get('https://medium.com/', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(body);
             $('a', 'div.site-main').each(function (index, aa) {
-                urls.push(aa.attribs.href);
-
+                let d = {
+                    "S.No": index,
+                    "href": aa.attribs.href
+                };
+                urls.push(d);
             });
-            console.log(urls);
-            console.log(urls.length);
+            console.log("Total No of href :", urls.length);
+            var jsonUrls = JSON.stringify(urls);
+            try {
+                let csv = json2csv({ data: urls, fields: fields });
+                fs.writeFile('file.csv', jsonUrls, function (err) {
+                    if (err) throw err;
+                    console.log('file saved');
+                });
+            } catch (error) {
+                console.log(error);
+            }
         }
     });
 });
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
-exports = module.exports = app;
